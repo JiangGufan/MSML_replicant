90f79f6fd5f5b5a6bdbeee0bfb5dedde3f3606d7
JiangGufan <120249579+JiangGufan@users.noreply.github.com>
2024-04-22 15:16:26 +0800

    Delete papers/Clustering_via_Dual_Divergence_Maximization directory

--- papers/Clustering_via_Dual_Divergence_Maximization/clusters_optimizer_base.py
+++
@@ -1,8 +0,0 @@
-
-
-class ClustersOptimizerBase:
-
-    def __init__(self, num_clusters=2, seed=0, is_debug=False):
-        self.num_clusters = num_clusters
-        self.seed = seed
-        self.is_debug = is_debug
